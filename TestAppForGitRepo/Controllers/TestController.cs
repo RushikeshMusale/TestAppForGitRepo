629e04b2e4db400f194d7d3f4c5c1661c5c100d8
Rushikesh Musale <rushikeshmusale@gmail.com>
2020-09-12 18:59:40 +0530

    Initial Commit - to display Simple Hello

---
+++ TestAppForGitRepo/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+    ***REMOVED***
+        public IActionResult Index()
+        ***REMOVED***
+            return Content("Simple Hello");
+***REMOVED***
+***REMOVED***
+***REMOVED***
